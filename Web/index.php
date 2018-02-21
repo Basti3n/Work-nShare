408873b433dee99291b798777b743a6c82b08781
Wiirlak <36702513+Wiirlak@users.noreply.github.com>
2018-02-21 16:49:57 +0100

    Coucou
    Cest ca la description

--- Web/index.php
+++ Web/index.php
@@ -5,7 +5,7 @@
 		<link rel="stylesheet" type="text/css" href="CSS/index_style.css">
 	</head>
 	<body>
-		<h1>En construction !!!!!</h1>
+		<h1 class="animated fadeInDown">En construction !!!!!</h1>
 		<script type="text/javascript" src="JS/index_script.js"></script>
 	</body>
 </html>
