477975fe5d07230df8178c62158f2282fc0f16c8
Wiirlak <36702513+Wiirlak@users.noreply.github.com>
2018-02-23 11:50:43 +0100

    edit footer

--- Web/footer.php
+++ Web/footer.php
@@ -1,18 +1,20 @@
 <?php require "header.php" ?>
-<footer>
-  <div class="sticky-footer">
 
+<footer class="sticky-footer">
+  <div class="container">
+    <div class="row">
+      <ul>
+        <li> <a href="about.php">about</a> </li>
+        <li> <a href="admin.php">admin</a> </li>
+        <li> <a href="cgu.php">cgu</a> </li>
+        <li> <a href="event.php">event</a> </li>
+        <li> <a href="index.php">index</a> </li>
+        <li> <a href="profil.php">profil</a> </li>
+        <li> <a href="service.php">service</a> </li>
+        <li> <a href="signup.php">signup</a> </li>
+        <li> <a href="subscribe.php">subscribe</a> </li>
+        <li> <a href="ticket.php">ticket</a> </li>
+      </ul>
+    </div>
   </div>
-  <ul>
-    <li> <a href="about.php">about</a> </li>
-    <li> <a href="admin.php">admin</a> </li>
-    <li> <a href="cgu.php">cgu</a> </li>
-    <li> <a href="event.php">event</a> </li>
-    <li> <a href="index.php">index</a> </li>
-    <li> <a href="profil.php">profil</a> </li>
-    <li> <a href="service.php">service</a> </li>
-    <li> <a href="signup.php">signup</a> </li>
-    <li> <a href="subscribe.php">subscribe</a> </li>
-    <li> <a href="ticket.php">ticket</a> </li>
-  </ul>
 </footer>
