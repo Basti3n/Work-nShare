651d7e03fb7575a9074943e2f4d26f755ec19473
Wiirlak <36702513+Wiirlak@users.noreply.github.com>
2018-03-29 18:29:32 +0200

    About
    About:
     - Need to add table in DB
     - Finished

--- Web/about.php
+++ Web/about.php
@@ -1,3 +1,8 @@
+<?php
+  require "conf.inc.php";
+  require "function.php";
+  include "object/spaces.php";
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -11,33 +16,46 @@
       <div class="inner">
         <div class="container">
           <div class="row">
-            <?php for ($i=0; $i < 3; $i++) {
-              echo '
-                <div class="col-lg-4">
-                  <img style="border-radius:50%;" src="IMG/bastille.jpg" alt="Generic placeholder image" width="140" height="140">
-                  <h2>Bastille</h2>
-                  <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-                  <p><a class="btn btn-secondary" href="about.php#bastille" role="button">View details »</a></p>
-                </div>
-              ';
-            } ?>
+            <?php
+            $db = connectDb();
+            $mng = new SpaceMng($db);
+            $spaces = $mng->getAllSpaces();
+            foreach ($spaces as $key => $space) {
+              if ($space->isDeleted()==0) {
+                $name = preg_replace('/[\s\']+/', '', $space->NameOfSpace());
+                echo '
+                  <div class="col-lg-4">
+                    <img style="border-radius:50%;" src="IMG/'.noAccents(utf8_encode ($name)).'.jpg" alt="Generic placeholder image" width="140" height="140">
+                    <h2>'.utf8_encode ($space->NameOfSpace()).'</h2>
+                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                    <p><a class="btn btn-secondary" href="about.php#'.utf8_encode ($space->NameOfSpace()).'" role="button">View details »</a></p>
+                  </div>
+                ';
+              }
+            }
+            ?>
           </div>
 
-          <?php for ($i=0; $i < 3; $i++) {
-            echo '
-            <hr>
+          <?php
+          foreach ($spaces as $key => $space) {
+            if ($space->isDeleted()==0) {
+              $name = preg_replace('/[\s\']+/', '', $space->NameOfSpace());
+              echo '
+              <hr>
 
-            <div class="row">
-              <div class="col-md-7">
-                <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It\'ll blow your mind.</span></h2>
-                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+              <div class="row">
+                <div class="col-md-7">
+                  <h2 class="featurette-heading">'.utf8_encode ($space->NameOfSpace()).'. <span class="text-muted">It\'ll blow your mind.</span></h2>
+                  <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                </div>
+                <div class="col-md-5">
+                  <img class="img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 500px;" src="IMG/'.noAccents(utf8_encode ($name)).'.jpg" data-holder-rendered="true">
+                </div>
               </div>
-              <div class="col-md-5">
-                <img class="img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 500px;" src="IMG/bastille.jpg" data-holder-rendered="true">
-              </div>
-            </div>
-            ';
-          } ?>
+              ';
+            }
+          }
+          ?>
 
 
 
--- Web/function.php
+++ Web/function.php
@@ -18,6 +18,16 @@ function showArray($array){
      return $db;
  }
 
+ function noAccents($str, $charset='utf-8')
+ {
+     $str = htmlentities($str, ENT_NOQUOTES, $charset);
+
+     $str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
+     $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str); // pour les ligatures e.g. '&oelig;'
+     $str = preg_replace('#&[^;]+;#', '', $str); // supprime les autres caractères
+
+     return $str;
+ }
 
  function exist($element, $table, $column){
     $db = connectDb();
