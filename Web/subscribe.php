6147d691be589a1c5ceef666ac5ad2d5b6324729
Wiirlak <36702513+Wiirlak@users.noreply.github.com>
2018-05-08 21:01:47 +0200

    starting subscribe

--- Web/subscribe.php
+++ Web/subscribe.php
@@ -22,6 +22,53 @@ require_once 'object/subscription.php';
         $mng = new SubscriptionMng($db);
         $subscriptions = $mng->getAll();
          ?>
+
+         <table class="table table-striped">
+           <thead class="thead-light">
+             <tr>
+               <th scope="col"></th>
+             <?php
+             $perm =[];
+             foreach ($subscriptions as $key => $value) {
+               foreach (array_keys($value->right("getAll")) as $keykey => $id) {
+                 if(!in_array($id,$perm) )
+                  array_push($perm,$id);
+               }
+               echo "<th scope='col'>".utf8_encode($value->name())."</th>\n";
+             }
+             print_r($perm);
+              ?>
+              </tr>
+             <!--
+             <tr>
+               <th scope="col">#</th>
+               <th scope="col">Jour</th>
+               <th scope="col" class="col-md-8">Heures</th>
+               <th scope="col" class="col-md-2">Facture</th>
+             </tr>
+           -->
+           </thead>
+           <tbody>
+             <?php
+             foreach ($perm as $key => $value) {
+               echo "
+               <tr>
+                 <th scope='row'>".$value."</th>
+                 <td>"."lol"."</td>
+                 <td>Vous êtes allé à <b>"."lol"."</td>
+               </tr>";
+             }
+
+              ?>
+             <!--
+             <tr>
+               <th scope='row'>".$value["idAccess"]."</th>
+               <td>".date('j\/m\/Y', strtotime($value["dateAccess"]))."</td>
+               <td>Vous êtes allé à <b>".nameOfSpace($value["idSpace"])."</b> de ".date('G\h i', strtotime($value["dateAccess"]))." à ".date('G\h i', strtotime($value["dateExit"]))."</td>
+             </tr>
+           -->
+           </tbody>
+         </table>
       </div>
     </div>
     <?php require "footer.php"; ?>
